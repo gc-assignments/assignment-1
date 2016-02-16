938ec8d4e1a76854f0df147922fef57cde75508d
Slevin Zhang <4ce111@gmail.com>
2016-02-16 13:18:35 -0500

    Build front-end

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home', shrink: false });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services', shrink: true });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects', shrink: true });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About', shrink: true });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact', shrink: true });
 });
 
 module.exports = router;
