1d351cc492654a49675fa5ba5dbf5c91dae079d7
Slevin Zhang <4ce111@gmail.com>
2016-02-16 19:10:48 -0500

    Handle form submit basic

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,13 @@ router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact', shrink: true });
 });
 
+router.post('/contact', function(req, res, next) {
+  console.log(req.body);
+  if (req.body.name && req.body.phone &&
+      req.body.email && req.body.message)
+    res.sendStatus(200);
+  else
+    res.sendStatus(500);
+});
+
 module.exports = router;
